1e01b705ebc15f9c7fdf8079ace0232b07a64c74
Felix Xavier Amaladoss <famaladoss@ebi.ac.uk>
2019-07-03 15:29:11 +0100

    Adds setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	  name='ecpcli',
+      version='1.0.0',
+      description='ECP command line client',
+      url='https://gitlab.ebi.ac.uk/sd/ecp/ecp-cli',
+      author=' Gianni Dalla Torre',
+      author_email='',
+      license='Apache License 2.0',
+      packages=['.'],
+      zip_safe=False
+	  )
